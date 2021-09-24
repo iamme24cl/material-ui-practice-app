1112c05e07fae123ca2ea3ff1343891c3df6c060
Chandra Lama <iamme24cl@gmail.com>
2021-09-24 10:43:41 -0700

    initial commmit --- installed react-router-dom and material-ui, deleted unused files, created Header component with material-ui AppBar

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/ui/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Hello!
+    </div>
+  );
+}
+
+export default App;
+
+
